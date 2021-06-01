1265d35aa9587d66db393c70cb294488597eb101
Flabib <fahdilabib@gmail.com>
2021-06-01 16:31:36 +0700

    implement timber

--- app/src/main/java/academy/bangkit/quiport/MyApplication.kt
+++ app/src/main/java/academy/bangkit/quiport/MyApplication.kt
@@ -6,14 +6,23 @@ import academy.bangkit.quiport.core.di.networkModule
 import academy.bangkit.quiport.core.di.repositoryModule
 import academy.bangkit.quiport.di.useCaseModule
 import academy.bangkit.quiport.di.viewModelModule
+import academy.bangkit.quiport.utils.ReleaseTree
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import timber.log.Timber
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
+        }
+
         startKoin {
             androidLogger(Level.NONE)
             androidContext(this@MyApplication)

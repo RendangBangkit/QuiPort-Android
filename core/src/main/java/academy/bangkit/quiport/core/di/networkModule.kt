92593afaabe4fbc6f153203ac415c9f6c5d82957
Flabib <fahdilabib@gmail.com>
2021-06-01 16:56:32 +0700

    implement ssl certificate pinner

--- core/src/main/java/academy/bangkit/quiport/core/di/networkModule.kt
+++ core/src/main/java/academy/bangkit/quiport/core/di/networkModule.kt
@@ -1,28 +1,46 @@
 package academy.bangkit.quiport.core.di
 
 import academy.bangkit.quiport.core.BuildConfig
+import academy.bangkit.quiport.core.BuildConfig.BASE_URL
 import academy.bangkit.quiport.core.data.source.remote.network.ApiService
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.CertificatePinner
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.URI
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = URI(BASE_URL).host
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/0vhur4rv1XMbO/Bzg7SUicSLyTsOoBfhpdTyK3yifDQ==")
+            .add(hostname, "sha256/rUCrPVDhSTsRzNk3Kd31x/AJ1FxCyuKlGooIEk9epy8=")
+            .add(hostname, "sha256/PJepcSEhCduoNGbeFk/D8M63d1+xi002VfGxM1uK6s8=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(get())
             .build()
+
         retrofit.create(ApiService::class.java)
     }
 }

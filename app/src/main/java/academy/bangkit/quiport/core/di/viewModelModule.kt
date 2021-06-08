93dfe335dde6b62f6327bb7455808573831b0f29
Flabib <fahdilabib@gmail.com>
2021-06-08 17:48:44 +0700

    add double tap to exit support

--- app/src/main/java/academy/bangkit/quiport/core/di/viewModelModule.kt
+++ app/src/main/java/academy/bangkit/quiport/core/di/viewModelModule.kt
@@ -1,6 +1,5 @@
 package academy.bangkit.quiport.core.di
 
-import academy.bangkit.quiport.presentation.example.ExampleViewModel
 import academy.bangkit.quiport.presentation.main.components.reportList.ReportListViewModel
 import academy.bangkit.quiport.presentation.main.components.reportMain.ReportMainViewModel
 import academy.bangkit.quiport.presentation.message.MessageViewModel
@@ -9,7 +8,6 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MessageViewModel(get()) }
-    viewModel { ExampleViewModel(get()) }
     viewModel { ReportMainViewModel(get()) }
     viewModel { ReportListViewModel(get()) }
 }
--- app/src/main/java/academy/bangkit/quiport/presentation/main/MainActivity.kt
+++ app/src/main/java/academy/bangkit/quiport/presentation/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package academy.bangkit.quiport.presentation.main
 
+import academy.bangkit.quiport.R
 import academy.bangkit.quiport.core.adapter.ReportSectionsPagerAdapter
 import academy.bangkit.quiport.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +29,23 @@ class MainActivity : AppCompatActivity() {
             viewpagerContainer.adapter = sectionsPagerAdapter
             bottomBar.setupWithViewPager2(viewpagerContainer)
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            finishAffinity()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Snackbar.make(binding.root, resources.getString(R.string.double_press), Snackbar.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed( {
+            doubleBackToExitPressedOnce = false
+        }, DOUBLE_TAP_DELAY)
+    }
+
+    companion object {
+        private const val DOUBLE_TAP_DELAY = 2000L
     }
 }
